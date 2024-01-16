70ead69789746fbca962476cfeff36ded660ae86
Caio <Caio@DESKTOP-4CPCOVM>
2024-01-16 10:52:44 -0300

    Corrigido maximo caracteres template documento

--- src/app/entities/crud/template-documento/components/template-documento-atualizar/template-documento-atualizar.component.ts
+++ src/app/entities/crud/template-documento/components/template-documento-atualizar/template-documento-atualizar.component.ts
@@ -57,7 +57,7 @@ export class TemplateDocumentoAtualizarComponent implements OnInit, OnDestroy {
       [
         Validators.required,
         Validators.minLength(9),
-        Validators.maxLength(5000),
+        Validators.maxLength(50000),
       ],
     ],
   });
