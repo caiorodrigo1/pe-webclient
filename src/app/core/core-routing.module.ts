005fbe7858782f320620550bd831be9cbe2e7975
Caio <Caio@DESKTOP-4CPCOVM>
2024-01-17 14:57:19 -0300

    Teste ci azure

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
       import('./../entities/entity.module').then((m) => m.EntityModule),
   },
 ];
-
+//ok
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [],
